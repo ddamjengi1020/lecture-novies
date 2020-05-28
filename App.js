0c287cf21c80c3863b80e67776f869b7e7f95f33
ddamjengi1020 <silluat11@gmail.com>
2020-05-28 23:56:22 +0900

    AppLoading API in expo testing

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { AppLoading } from "expo";
+import { Text, Image } from "react-native";
+import { Asset } from "expo-asset";
 
+const cacheImages = (images) => {
+  return images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(images).downloadAsync();
+    }
+  });
+};
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = () => {
+    const images = cacheImages([
+      "https://images.unsplash.com/photo-1590634331662-660d6992a9f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    ]);
+    return Promise.all([...images]);
+  };
+  const onFinish = () => setIsReady(true);
+  return isReady ? (
+    <Text>Its Ready!</Text>
+  ) : (
+    <AppLoading
+      startAsync={loadAssets}
+      onError={console.error}
+      onFinish={onFinish}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
